a9fbc9d7a1d62bb71fad5e53235d7b42258a2815
Charles Hughes <charles@thousandeyes.com>
2018-08-18 19:52:05 -0700

    Revert "modify button layout"
    This reverts commit fe3a868b90eec0dfed754d97241f268f4956ee4e.

--- keyboard/atreus/keymap_common.h
+++ keyboard/atreus/keymap_common.h
@@ -39,18 +39,34 @@ extern const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS];
 extern const uint16_t fn_actions[];
 
 #define KEYMAP_PCBDOWN( \
-  K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
-  K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
-  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-  K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, \
+  K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A \
 ) {                                                                     \
-  { K09, K08, K07, K06, K05, K26, K04, K03, K02, K01, K00 }, \
-  { K19, K18, K17, K16, K15, K25, K14, K13, K12, K11, K10 }, \
-  { K2B, K2A, K29, K28, K27, K36, K24, K23, K22, K21, K20 }, \
-  { K3B, K3A, K39, K38, K37, K35, K34, K33, K32, K31, K30 } \
+  { K09, K08, K07, K06, K05, KC_NO, K04, K03, K02, K01, K00 }, \
+  { K19, K18, K17, K16, K15, KC_NO, K14, K13, K12, K11, K10 }, \
+  { K29, K28, K27, K26, K25, K35,   K24, K23, K22, K21, K20 }, \
+  { K3A, K39, K38, K37, K36, K34,   K33, K32, K31, K30, K2A } \
 }
 
+#define KEYMAP_PCBUP( \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, \
+  K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A \
+) {                                                                     \
+  { K00, K01, K02, K03, K04, KC_NO, K05, K06, K07, K08, K09 }, \
+  { K10, K11, K12, K13, K14, KC_NO, K15, K16, K17, K18, K19 }, \
+  { K20, K21, K22, K23, K24, K34,   K25, K26, K27, K28, K29 }, \
+  { K2A, K30, K31, K32, K33, K35,   K36, K37, K38, K39, K3A }      \
+}
+
+#ifdef PCBDOWN
 #define KEYMAP KEYMAP_PCBDOWN
+#else
+#define KEYMAP KEYMAP_PCBUP
+#endif
 
 #define FN_LAYER   KEYMAP(SHIFT(KC_1), SHIFT(KC_2), SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS), \
                             KC_PGUP, KC_7, KC_8, KC_9, SHIFT(KC_8),       \
